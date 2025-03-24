7959c2f6581e39f4c718642d2a7e3070e474f98a
Adam Zimny <adamczyk97@op.pl>
2025-03-24 22:10:23 +0100

    Initial files from generator project spring initializr

---
+++ src/main/java/com/az/project/backend_chat_api/BackendChatApiApplication.java
@@ -0,0 +1,13 @@
+package com.az.project.backend_chat_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/az/project/backend_chat_api/BackendChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.project.backend_chat_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
